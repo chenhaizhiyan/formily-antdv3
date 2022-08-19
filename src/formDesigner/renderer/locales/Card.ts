503a0e3988ab0d967ef0972352c53911d5bac721
chengq <chengq@thinkive.com>
2022-08-19 10:41:39 +0800

    feat: Card改造，拆成上下两部分，title右侧插槽支持拖入控件

--- src/formDesigner/renderer/locales/Card.ts
+++ src/formDesigner/renderer/locales/Card.ts
@@ -42,3 +42,93 @@ export const Card = {
     },
   },
 }
+
+export const CardTitle = {
+  'zh-CN': {
+    title: '卡片标题',
+    settings: {
+      'x-component-props': {
+        type: '类型',
+        title: '标题',
+        extra: '右侧扩展',
+        cardTypes: [
+          { label: '内置', value: 'inner' },
+          { label: '默认', value: '' },
+        ],
+      },
+    },
+  },
+  'en-US': {
+    title: 'Card Title',
+    settings: {
+      'x-component-props': {
+        type: 'Type',
+        title: 'Title',
+        extra: 'Extra',
+        cardTypes: [
+          { label: 'Inner', value: 'inner' },
+          { label: 'Default', value: '' },
+        ],
+      },
+    },
+  },
+  'ko-KR': {
+    title: '카드',
+    settings: {
+      'x-component-props': {
+        type: '타입',
+        title: '제목',
+        extra: '추가 항목',
+        cardTypes: [
+          { label: '안쪽', value: 'inner' },
+          { label: '기본', value: '' },
+        ],
+      },
+    },
+  },
+}
+
+export const CardBody = {
+  'zh-CN': {
+    title: '卡片内容',
+    settings: {
+      'x-component-props': {
+        type: '类型',
+        title: '标题',
+        extra: '右侧扩展',
+        cardTypes: [
+          { label: '内置', value: 'inner' },
+          { label: '默认', value: '' },
+        ],
+      },
+    },
+  },
+  'en-US': {
+    title: 'Card',
+    settings: {
+      'x-component-props': {
+        type: 'Type',
+        title: 'Title',
+        extra: 'Extra',
+        cardTypes: [
+          { label: 'Inner', value: 'inner' },
+          { label: 'Default', value: '' },
+        ],
+      },
+    },
+  },
+  'ko-KR': {
+    title: '카드',
+    settings: {
+      'x-component-props': {
+        type: '타입',
+        title: '제목',
+        extra: '추가 항목',
+        cardTypes: [
+          { label: '안쪽', value: 'inner' },
+          { label: '기본', value: '' },
+        ],
+      },
+    },
+  },
+}
